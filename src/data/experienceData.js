053c567a432848e503253b58b1d3460e8a398ef1
Ravi Kiran Vadde <vadderavikiran@gmail.com>
2024-08-04 16:57:46 -0700

    updated experience

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,20 +1,27 @@
 export const experienceData = [
     {
         id: 1,
+        company: 'Denken Solutions Inc.',
+        jobtitle: 'Java Developer Intern',
+        startYear: '2024',
+        endYear: 'Till Date'
+    },
+    {
+        id: 2,
         company: 'Texas Tech University',
         jobtitle: 'Student Assistant',
         startYear: '2022',
         endYear: '2024'
     },
     {
-        id: 2,
+        id: 3,
         company: 'Tata Consultancy Services',
         jobtitle: 'Full Stack Java Developer',
         startYear: '2019',
         endYear: '2022'
     },
     {
-        id: 3,
+        id: 4,
         company: 'Tata Consultancy Services',
         jobtitle: 'Java Developer',
         startYear: '2019',
--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,7 @@ import image from '../assets/images/profile.jpg'
 export const headerData = {
     name: 'Ravi Kiran Vadde',
     title: "Full Stack Java Developer",
-    desciption:"Full Stack Developer and Technical Lead with expertise in Java, Spring Boot, and microservices architecture, complemented by a recent Master's in Computer Science from Texas Tech University, showcasing a robust skill set in software development, leadership, and continuous learning.",
+    desciption:"Full Stack Developer and Technical Lead with expertise in Java, Spring Boot, and microservices architecture, complemented by a recent Master's in Computer Science from Texas Tech University currently working as Java developer Intern with Denken Solutions Inc., showcasing a robust skill set in software development, leadership, and continuous learning.",
     image: image,
     resumePdf: resume
 }
