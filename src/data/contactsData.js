b799ce189fb67f08b104193b1716b10d6f6dc9f8
Ravi Kiran Vadde <vadderavikiran@gmail.com>
2024-11-14 08:33:49 -0500

    modified experience and updated resume

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
     email: 'vadderavikiran@gmail.com',
     phone: '+1 (806) 702-2270',
-    address: 'Laguna Hills, California, United States - 92653 ',
+    address: 'Dallas, Texas, United States - 75204 ',
 
     sheetAPI: ''
 }
